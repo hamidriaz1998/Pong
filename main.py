c9fa6ca1797a0efd19af208ba21b10318d6cf995
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-07 18:26:56 +0500

    Add paddle instances for player 1 and player 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 # Setup Screen
 screen = Screen()
@@ -7,9 +8,13 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.title("Pong")
 
+# Create instances
+player1 = Paddle("right")
+player2 = Paddle("left")
+
 # Listen for keystrokes
 screen.listen()
-# Control for player 2
+# Control for player 1
 screen.onkey("Up")
 screen.onkey("Down")
 # Control for player 2
