7ddf3d6ceeac50cce6b9a48a62d8f0519a64c01d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-07 18:33:31 +0500

    Add movement control to Paddle class

--- paddle.py
+++ paddle.py
@@ -8,10 +8,13 @@ class Paddle(Turtle):
     def __init__(self, side):
         super().__init__()
         self.side = side
+        self.positions = P1POSITION if self.side == "right" else P2POSITION
+        self.segments = []
         self.createPaddle()
+        self.head = self.segments[0]
 
     def createPaddle(self):
-        for i in P1POSITION if self.side == "left" else P2POSITION:
+        for i in self.positions:
             self.addSegment(i)
 
     def addSegment(self, postion):
@@ -21,3 +24,18 @@ class Paddle(Turtle):
         newSegment.color("white")
         newSegment.goto(x, y)
         self.segments.append(newSegment)
+
+    # Movement Control
+    def move(self):
+        for segIdx in range(len(self.segments) - 1, 0, -1):
+            x, y = self.segments[segIdx - 1].pos()
+            self.segments[segIdx].goto(x, y)
+        self.head.forward(20)
+
+    def up(self):
+        self.head.setheading(90)
+        self.move()
+
+    def down(self):
+        self.head.setheading(270)
+        self.move()
