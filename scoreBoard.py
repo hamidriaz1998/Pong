f300d5cdb3e63b236fee55ea9444aa5e3dfdba60
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-08 12:12:08 +0500

    Refactor printScores and printGameOver methods

--- scoreBoard.py
+++ scoreBoard.py
@@ -8,13 +8,13 @@ class ScoreBoard(Turtle):
         super().__init__()
         self.p1Score = 0
         self.p2Score = 0
-        self.printScores()
-
-    def printScores(self):
-        self.reset()
         self.color("white")
         self.penup()
         self.hideturtle()
+        self.printScores()
+
+    def printScores(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.p1Score, align="center", font=FONT)
         self.goto(100, 200)
@@ -32,9 +32,6 @@ class ScoreBoard(Turtle):
         return self.p1Score == 5 or self.p2Score == 5
 
     def printGameOver(self):
-        self.reset()
-        self.color("white")
-        self.penup()
-        self.hideturtle()
+        self.clear()
         self.goto(0, 0)
         self.write("Game Over", align="center", font=FONT)
